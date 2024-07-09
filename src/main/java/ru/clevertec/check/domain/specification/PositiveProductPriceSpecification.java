501a3f50d2af0a18602dcf4a184b3a47108e6345
Eugene <compserv@yahoo.com>
2024-07-10 02:19:29 +0300

    complete first level task

---
+++ src/main/java/ru/clevertec/check/domain/specification/PositiveProductPriceSpecification.java
@@ -0,0 +1,23 @@
+package ru.clevertec.check.domain.specification;
+
+import ru.clevertec.check.domain.model.exception.GenericSpecificationException;
+import ru.clevertec.check.domain.model.valueobject.Price;
+import ru.clevertec.check.domain.model.valueobject.ProductName;
+import ru.clevertec.check.domain.specification.shared.AbstractSpecification;
+
+import java.math.BigDecimal;
+
+public class PositiveProductPriceSpecification extends AbstractSpecification<Price> {
+
+
+    @Override
+    public void check(Price item) {
+        if(!isSatisfiedBy(item))
+            throw new GenericSpecificationException("The price of the product should be positive. Current price: %s".formatted(item.value()));
+    }
+
+    @Override
+    public boolean isSatisfiedBy(Price item) {
+        return item.value().doubleValue() > 0;
+    }
+}
---
+++ src/main/java/ru/clevertec/check/infrastructure/output/file/ProductFileOutputAdapter.java
@@ -0,0 +1,37 @@
+package ru.clevertec.check.infrastructure.output.file;
+
+import ru.clevertec.check.application.ports.output.ProductOutputPort;
+import ru.clevertec.check.domain.model.valueobject.ProductPosition;
+import ru.clevertec.check.infrastructure.output.file.mapper.CSVStructureToProductPositionsMapper;
+import ru.clevertec.check.infrastructure.output.file.mapper.shared.CSVStructureToObjectMapper;
+import ru.clevertec.check.infrastructure.utils.CSVReader;
+import ru.clevertec.check.infrastructure.utils.SimpleCVSFileReader;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+import java.util.function.Predicate;
+
+public class ProductFileOutputAdapter implements ProductOutputPort {
+    private final String PRODUCT_LIST_RESOURCE_FILE_NAME = "products.csv";
+    private final CSVReader csvReader = new SimpleCVSFileReader();
+    CSVStructureToObjectMapper<ProductPosition> mapper = new CSVStructureToProductPositionsMapper();
+
+    @Override
+    public List<ProductPosition> findAll() throws URISyntaxException, IOException {
+        Predicate<String> notEmpty = line -> !line.isEmpty();
+        Predicate<String> startWithDigit = line -> Character.isDigit(line.charAt(0));
+
+        List<Predicate<String>> predicates = List.of(notEmpty, startWithDigit);
+        List<String[]> rawProductData = csvReader.readAndFilterRecords(PRODUCT_LIST_RESOURCE_FILE_NAME, predicates);
+
+        return mapper.map(rawProductData);
+    }
+
+    private List<String> readProductFileAsTextLines() throws IOException {
+        Path pathToProductsFile = Path.of("products.csv");
+        return Files.readAllLines(pathToProductsFile);
+    }
+}
